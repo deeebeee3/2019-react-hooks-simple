ae677f130261d94807bbe79cecfe6b2c1b01fbdf
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-06-06 10:27:39 +0100

    refactor class to func - convert componentDidUpdate to fetchResource, delete componentWillMount and render methods

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,31 +1,18 @@
 import React from "react";
 import axios from "axios";
 
-class ResourceList extends React.Component {
-  state = { resources: [] };
+const ResourceList = () => {
 
-  //componentDidMount only ever called one time 
-  async componentDidMount() {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+  fetchResource = async () => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
 
-    this.setState({resources: response.data});
-  }
+    this.setState({ resources: response.data });
+  };
 
-  //componentDidMount called everytime component rerendered
-  async componentDidUpdate(prevProps){
-    
-      //this check will stop the endless loop, has the resource type change? if so make another request
-      if(prevProps.resource !== this.props.resource){
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-
-        //this will cause an endless loop and cause componentDidUpdate to endlessly be called
-        this.setState({resources: response.data}); 
-      }
-  }
-
-  render() {
-    return <div>{this.state.resources.length}</div>;
-  }
-}
+  return <div>{this.state.resources.length}</div>;
+  
+};
 
 export default ResourceList;
