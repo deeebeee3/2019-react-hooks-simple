6c251cdd1ebe6cc60b3066731f10a11589ade09c
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-06-06 11:31:42 +0100

    move seperate function into its own file - name lowercase as its not a component

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,26 +1,7 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import useResources from './useResources';
 
-const useResources = resource => {
-  const [resources, setResources] = useState([]);
 
-  //useEffect will always be called whenever component rendered
-  //when component first gets rendered - the inner function will run
-  //between subsequent renders, if the value in the array (second arg) is different - the inner function will run
-  //if a second argument is not provided (array) then the inner function will run 100% of the time...
-  //so no array - 100% of time, an empty array - one time, an array with resource - runs only if different
-  useEffect(() => {
-    (async resource => {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/${resource}`
-      );
-
-      setResources(response.data);
-    })(resource);
-  }, [resource]);
-
-  return resources;
-};
 
 //props.resource destructured to resource - will either be 'posts' or 'todos'
 const ResourceList = ({ resource }) => {
