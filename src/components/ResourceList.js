835a0e7305061362aa7e13c841bd00a2f82e3445
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-06-06 10:21:59 +0100

    componentDidUpdate vs componentDidMount

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -4,10 +4,23 @@ import axios from "axios";
 class ResourceList extends React.Component {
   state = { resources: [] };
 
+  //componentDidMount only ever called one time 
   async componentDidMount() {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
 
     this.setState({resources: response.data});
+  }
+
+  //componentDidMount called everytime component rerendered
+  async componentDidUpdate(prevProps){
+    
+      //this check will stop the endless loop, has the resource type change? if so make another request
+      if(prevProps.resource !== this.props.resource){
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+        //this will cause an endless loop and cause componentDidUpdate to endlessly be called
+        this.setState({resources: response.data}); 
+      }
   }
 
   render() {
