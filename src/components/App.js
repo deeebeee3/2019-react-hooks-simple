9dd9491bcf29e23eed84243292ffb59a879eff86
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-06-06 09:38:03 +0100

    useState in detail

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 
 const App = () => {
+
+  //array destructuring  
   const [resource, setResource] = useState("posts");
 
+  //another example 
+  //const [currentCount, setCount] = useState(0);
+
+  //object destructuring
+  //const { something } = this.state;
+
+
+  //using setResource will cause component to automatically rerender
   return (
     <div>
       <div>
@@ -15,3 +25,8 @@ const App = () => {
 };
 
 export default App;
+
+//useState function returns a piece of state and a function to set it
+//when useState is first called the single argument is the initial state value
+//calling setResource will set the resource state and cause component to automatically rerender
+//const [resource, setResource] = useState("posts");
